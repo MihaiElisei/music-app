746c1ce9ff6afd055d161f128af7f08e63731215
Mihai <mihai.m.elisei@gmail.com>
2025-09-25 15:45:58 +0300

    Created fastapi project, connected db and created signup route

---
+++ server/routes/auth.py
@@ -0,0 +1,35 @@
+from fastapi import HTTPException, Depends
+import uuid
+import bcrypt
+from models.user import User
+from pydantic_shcemas.user_create import UserCreate
+from fastapi import APIRouter
+from database import get_db
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+@router.post("/signup")
+def signup_user(user: UserCreate, db: Session = Depends(get_db)):
+    
+    #check if user already exists
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+        
+    #create new user
+    hashed_pwd = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt(16))
+    new_user = User(
+        id=str(uuid.uuid4()),
+        name=user.name,
+        email=user.email,
+        password=hashed_pwd
+    )
+    #save user to db
+    db.add(new_user)
+    #commit the changes
+    db.commit()
+    #refresh the user instance
+    db.refresh(new_user)
+    #return the created user
+    return new_user
